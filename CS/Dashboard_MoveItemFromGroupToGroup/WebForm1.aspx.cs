cc94904719d4d1b2bf400886b9bb52b93dfec90c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Dashboard_MoveItemFromGroupToGroup/WebForm1.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using DevExpress.DashboardWeb;
+using DevExpress.DataAccess.ConnectionParameters;
+using DevExpress.DashboardCommon;
+
+namespace Dashboard_MoveItemFromGroupToGroup {
+    public partial class WebForm1 : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+        }
+
+        protected void ASPxDashboardViewer1_DashboardLoaded(object sender, 
+            DashboardLoadedWebEventArgs e) {
+            Dashboard dashboard = e.Dashboard;
+            ComboBoxDashboardItem comboBox = 
+                (ComboBoxDashboardItem)dashboard.Items["comboBoxDashboardItem1"];
+            TreeViewDashboardItem treeView = 
+                (TreeViewDashboardItem)dashboard.Items["treeViewDashboardItem1"];
+            DashboardItemGroup topGroup = 
+                (DashboardItemGroup)dashboard.Groups["dashboardItemGroup1"];
+
+            DashboardLayoutGroup root = e.Dashboard.LayoutRoot;;
+            DashboardLayoutItem comboBoxLayoutItem = root.FindRecursive(comboBox);
+            DashboardLayoutItem treeViewLayoutItem = root.FindRecursive(treeView);
+            DashboardLayoutGroup topGroupItem = root.FindRecursive(topGroup);
+            
+            comboBoxLayoutItem.MoveAbove(treeViewLayoutItem);            
+            comboBox.Group = topGroup;             
+        }
+
+        protected void ASPxDashboardViewer1_ConfigureDataConnection(object sender, 
+            ConfigureDataConnectionWebEventArgs e) {
+                if (e.ConnectionName == "WebsiteStatisticsDataConnection") {
+                XmlFileConnectionParameters parameters =
+                    (XmlFileConnectionParameters)e.ConnectionParameters;
+                string databasePath = Server.MapPath("App_Data/WebsiteStatisticsData.xml");                   
+                parameters.FileName = databasePath;
+            }
+        }
+    }
+}
---
+++ VB/Dashboard_MoveItemFromGroupToGroup/WebForm1.aspx.vb
@@ -0,0 +1,42 @@
+﻿Imports System
+Imports DevExpress.DashboardWeb
+Imports DevExpress.DataAccess.ConnectionParameters
+Imports DevExpress.DashboardCommon
+
+Namespace Dashboard_MoveItemFromGroupToGroup
+    Partial Public Class WebForm1
+        Inherits System.Web.UI.Page
+
+        Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+        End Sub
+
+        Protected Sub ASPxDashboardViewer1_DashboardLoaded(ByVal sender As Object, _
+                                           ByVal e As DashboardLoadedWebEventArgs)
+            Dim dashboard As Dashboard = e.Dashboard
+            Dim comboBox As ComboBoxDashboardItem =
+                CType(dashboard.Items("comboBoxDashboardItem1"), ComboBoxDashboardItem)
+            Dim treeView As TreeViewDashboardItem =
+                CType(dashboard.Items("treeViewDashboardItem1"), TreeViewDashboardItem)
+            Dim topGroup As DashboardItemGroup =
+                CType(dashboard.Groups("dashboardItemGroup1"), DashboardItemGroup)
+
+            Dim root As DashboardLayoutGroup = e.Dashboard.LayoutRoot
+            Dim comboBoxLayoutItem As DashboardLayoutItem = root.FindRecursive(comboBox)
+            Dim treeViewLayoutItem As DashboardLayoutItem = root.FindRecursive(treeView)
+            Dim topGroupItem As DashboardLayoutGroup = root.FindRecursive(topGroup)
+
+            comboBoxLayoutItem.MoveAbove(treeViewLayoutItem)
+            comboBox.Group = topGroup
+        End Sub
+
+        Protected Sub ASPxDashboardViewer1_ConfigureDataConnection(ByVal sender As Object, _
+                                           ByVal e As ConfigureDataConnectionWebEventArgs)
+            If e.ConnectionName = "WebsiteStatisticsDataConnection" Then
+                Dim parameters As XmlFileConnectionParameters =
+                    CType(e.ConnectionParameters, XmlFileConnectionParameters)
+                Dim databasePath As String = Server.MapPath("App_Data/WebsiteStatisticsData.xml")
+                parameters.FileName = databasePath
+            End If
+        End Sub
+    End Class
+End Namespace
